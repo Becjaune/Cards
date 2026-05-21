0c1939eff844fb4a2c23fee3ade6c8d98c8d5820
Laurent <laurentlemerle@me.com>
2026-05-21 22:45:41 +0200

    Update VCF files 05/21/2026 22:45:41

--- Global.ps1
+++ Global.ps1
@@ -6,7 +6,7 @@
   -CertThumbprint "240910F19838E6CA4E2D74B7D428A4D7274D1AD7" `
   -UpnFile "C:\Repos\Cards\comex.txt" `
   -OutputDir "C:\Repos\Cards\contacts" `
-  -IncludeFields "JobTitle","Company","Email","Mobile","FN", "N", "Department", "Address", "office", "BusinessPhones"
+  -IncludeFields "JobTitle","Company","Email","Mobile","FN", "N" # ,"Department", "Address", "office", "BusinessPhones"
 
 # Use inverse mode with explicit includes instead of excludes:
 #-IncludeFields "FN","N","Email","Mobile","Address" `
