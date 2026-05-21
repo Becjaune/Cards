52fa81cfa5acdc3b945a1191d8ec073e429f0edf
Laurent <laurentlemerle@me.com>
2026-05-21 22:49:02 +0200

    Update VCF files 05/21/2026 22:49:02

--- Global.ps1
+++ Global.ps1
@@ -4,7 +4,7 @@
   -TenantId "d0ae6336-5c35-4735-9703-03fb777bbd2b" `
   -ClientId "4203faaf-c609-4601-9645-71399763e2df" `
   -CertThumbprint "240910F19838E6CA4E2D74B7D428A4D7274D1AD7" `
-  -UpnFile "C:\Repos\Cards\comex.txt" `
+  -UpnFile "C:\Repos\Cards\upns.txt" `
   -OutputDir "C:\Repos\Cards\contacts" `
   -IncludeFields "JobTitle","Company","Email","Mobile","FN", "N" # ,"Department", "Address", "office", "BusinessPhones"
 
